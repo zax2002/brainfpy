8cca038e71f6b23b29aa4560be3cb3c4364524b8
zax2002 <zax2002@yandex.ru>
2020-05-10 00:27:26 +0300

    Full version
    Supports ~infinite nested loops, 30k cells, 256 looped velues, comments, output without \n per char

---
+++ full.py
@@ -0,0 +1,3 @@
+a=b=c=d=e=0
+f,g=input(),[0]*30000
+while(b<len(f)):exec(("|a+=1|a-=1|g[a]=(g[a]+1)%256|g[a]=(g[a]-1)%256|print(chr(g[a]),end='')|g[a]=ord(input())"+("|d,e=c,{}{}{} g[a]\nwhile e:b{}=1;e=f[b]!='{}'or c!=d;c+=[0,-1,1]['{}{}'.find(f[b])+1]"*2).format(*"not+]][   -[[]")).split("|")["><+-.,[]".find(f[b])+1]);b+=1
